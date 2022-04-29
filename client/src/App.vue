8f2ebb70832929a879dad2f0ec6a6d436940019a
Gianluca Riboldi <gian.ribo95@gmail.com>
2022-04-30 00:43:37 +0200

    front end

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {{ item }}
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      item: ""
+    }
+  },
+  async mounted() {
+    const res = await axios.get("api/bucketListItems/")
+    this.item = res.data
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
